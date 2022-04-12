7a461735989d27be1e7cd91216a043c21027cde0
JT Bergman <61445278+jtbergman@users.noreply.github.com>
2022-04-12 01:22:17 -0400

    Add files via upload

---
+++ incremental-multi-cloze/incremental-multi-cloze.js
@@ -0,0 +1,84 @@
+<script>
+  // What key is used to advance? 
+  let keyToPress = 'n';
+
+  // Store some necessary values for hiding clozes
+  let clozes;
+  let cloze_answers;
+  let next_cloze_to_show;
+
+
+  // Invoke once the back card is visible because the cloze elemens get reloaded
+  function populate_necessary_values() {
+    clozes = document.getElementsByClassName("cloze");
+    cloze_answers = [];
+    if (clozes) {
+      for (i = 0; i < clozes.length; i++) {
+        cloze_answers.push(clozes[i].innerHTML);
+      }
+    }
+  }
+
+
+  // Hides all the cloze elements and gives them a fix width as element width is a visual hint
+  function hide_clozes_from_idx(idx) {
+    next_cloze_to_show = idx
+    for (i = next_cloze_to_show; i < clozes.length; i++) {
+      clozes[i].innerHTML = "[...]";
+    } 
+  }
+
+
+  // Add observer for `back-of-card` to initially hide clozes
+  const observer = new MutationObserver(function(mutations_list) {
+    mutations_list.forEach(function(mutation) {
+      mutation.addedNodes.forEach(function(added_node) {
+        if(added_node.id == 'back-of-card') {
+          populate_necessary_values();
+          hide_clozes_from_idx(1);
+        }
+      });
+    });
+  });
+
+  observer.observe(document.querySelector("body"), { subtree: true, childList: true });
+
+
+  // Invokes "show answer" programmatically
+  function show_answer() {
+    if (typeof(py) !== "undefined") {
+      py.link('ans');
+      return;
+    }
+
+    if (typeof(pycmd) !== "undefined") {
+      pycmd('ans'); 
+      return;
+    }
+  }
+
+
+  // Determines the appropriate behavior for when `keyToPress` is press
+  function keypressHandler() {
+    if (document.getElementById('front-of-card')) {
+      show_answer();
+      return;
+    }
+
+    if (next_cloze_to_show >= clozes.length) {
+      return;
+    }
+
+    clozes[next_cloze_to_show].innerHTML = cloze_answers[next_cloze_to_show];
+    next_cloze_to_show += 1;
+  }
+
+
+  // Register event listener for `keyToPress` keypress
+  document.addEventListener('keypress', function(event) {
+      var key = event.key || event.keyCode; 
+      if (event.key == keyToPress) {
+        keypressHandler();
+      }
+  })
+</script>
